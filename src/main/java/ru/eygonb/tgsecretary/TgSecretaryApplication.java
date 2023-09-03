447c8b4841da2dc19b3e6943dd7749598281a6bf
Eygonb <eygonb@gmail.com>
2023-09-03 20:29:57 +0300

    Add base spring structure

---
+++ src/main/java/ru/eygonb/tgsecretary/TgSecretaryApplication.java
@@ -0,0 +1,13 @@
+package ru.eygonb.tgsecretary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgSecretaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgSecretaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/eygonb/tgsecretary/TgSecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eygonb.tgsecretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgSecretaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
